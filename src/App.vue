16cce55dd6380d137fb753899a0abdeab9b825ab
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-07-07 19:49:06 +0200

    Añadidas variables colores y tipografias

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
 </template>
 
 <style lang="scss">
+$color-principal:#5A189A;
+$color-secundario:#9D4EDD;
+$color-ternario:#C77DFF;
+$color-oscuro:#211922;
+$color-blanco:#ffffff;
+$tipografia-titulos: 'Rubik', sans-serif;
+$tipografia-suptitulos: 'Cormorant Garamond', serif;
+$tipografia-parrafos:  'Yrsa', serif;
 
 
 
