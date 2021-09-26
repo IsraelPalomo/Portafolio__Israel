fcc78534c6e777eae2cb4eceae159c89d96f4f34
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-26 19:16:53 +0200

    Añadida vista blog en desarollo

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -3,7 +3,7 @@
 		<img src="../assets/desarrollo.png" alt="img blog en desarollo" />
 		<div class="container">
 			<h2>Blog Actualmente En Desarrollo</h2>
-			<button>Volver al inicio</button>
+			<router-link to="/"><button>Volver al inicio</button></router-link>
 		</div>
 	</div>
 </template>
@@ -21,8 +21,8 @@ export default {};
 	height: 100vh;
 	background-color: #1b1b1b;
 	img {
-		width: 35rem;
-		height: 20rem;
+		width: 30rem;
+		height: 17rem;
 		margin-left: -5rem;
 	}
 	h2 {
@@ -30,8 +30,14 @@ export default {};
 			"Open Sans", "Helvetica Neue", sans-serif;
 		color: white;
 		margin-bottom: 5rem;
+		margin-left: auto;
+		margin-right: auto;
+		width: 70%;
+		text-align: center;
 	}
 	button {
+		margin-left: auto;
+		margin-right: auto;
 		margin: 2rem 2rem;
 		padding: 0.5rem;
 		border: none;
@@ -54,6 +60,38 @@ export default {};
 	}
 	.container {
 		text-align: center;
+	}
+}
+@media (max-width: 2000px) {
+}
+@media (max-width: 1200px) {
+	.Blog {
+		display: grid;
+		grid-template-columns: 1fr;
+		justify-content: center;
+		text-align: center;
+		h2 {
+			margin-bottom: 1rem;
+			margin-top: -9rem;
+		}
+		img {
+			margin: 0 auto;
+			padding: 0;
+		}
+	}
+}
+@media (max-width: 900px) {
+}
+@media (max-width: 900px) {
+}
+@media (max-width: 650px) {
+}
+@media (max-width: 450px) {
+	.Blog {
+		img {
+			width: 17rem;
+			height: 10rem;
+		}
 	}
 }
 </style>
