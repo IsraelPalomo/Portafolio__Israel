5dc1f688df0363a84654fbbe033175bf449b9a3f
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-28 08:58:59 +0200

    Configurado EsLint junto a Prettier

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
 
-import AOS from 'aos';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "aos/dist/aos.css";
 
 createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app');
+	.use(store)
+	.use(router)
+	.mount("#app");
 
 AOS.init();
