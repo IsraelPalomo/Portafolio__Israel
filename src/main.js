7bf353e56fc62bea1c6675372248292aafae3c9f
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-09-28 08:17:45 +0200

    Añadido y configurado EsLint

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { createApp } from 'vue';
+
+import AOS from 'aos';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'aos/dist/aos.css';
 
 createApp(App)
-	.use(store)
-	.use(router)
-	.mount("#app");
+  .use(store)
+  .use(router)
+  .mount('#app');
 
 AOS.init();
